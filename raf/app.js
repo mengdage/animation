6c9a1bf7b3e53e9ba1eaab12b8b9c84e78564527
meng <wulalameng@gmail.com>
2017-09-03 13:59:34 -0400

    fix bug

--- raf/app.js
+++ raf/app.js
@@ -2,12 +2,12 @@ window.addEventListener('keydown', keypressHandler);
 function keypressHandler(e) {
   console.log('window: ', e.key, e.code);
 }
-const box = document.getElementsByClassName('box')[0];
+const box = document.querySelector('.box');
 let start = null;
 function move(timestamp) {
   if(!start) start = timestamp;
   console.log(timestamp);
-  box.style.width = Math.max((timestamp-start)/10, 100) + 'px';
+  box.style.width = box.clientWidth + 1 + 'px';
   if(timestamp-start < 2000) {
     requestAnimationFrame(move);
   }
